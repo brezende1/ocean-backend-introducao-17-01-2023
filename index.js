a1bdc4c135d2bf97b4eac107738bd6557918a59a
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-18 15:07:04 -0300

    URL local comentada

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
+// const url = "mongodb://localhost:27017";
 const url = "mongodb+srv://admin:zYXlPnsqZbydsbMX@cluster0.ik86v.mongodb.net";
 
 const dbName = "ocean_database_18_01_2022";
