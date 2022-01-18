43460d00f22f460ed6c8838e7aa4fb49885f863f
Hamom Silva <hamomgomer947@gmail.com>
2022-01-18 14:51:18 -0300

    Reativando banco de dados e adcionando url do mong

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 const url = "mongodb://localhost:27017";
-// const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
+const url = "mongodb+srv://admin:quL9va4.tui@pNk@cluster0.ik86v.mongodb.net";
 
 const dbName = "ocean_database_18_01_2022";
 
 async function main() {
     // Conexão com o Banco de Dados
-/*
+
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("heroes");
-*/
-    // Aplicação em Express
 
-    const collection = undefined;
+    // Aplicação em Express
 
     const app = express();
 
